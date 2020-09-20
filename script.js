2a4acca01f6b2355b83c0ff0ed88c542f7c01e6b
Kelly Stone <kellystone916@gmail.com>
2020-09-19 21:58:19 -0700

    cleaned up code & deleted extra comments

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers = "1234567890"
 var special = "~!@#$%^&*()_-"
-
 var characters = ""
 var password = ""
 
+
+// Password Criteria 
 
 alert ("Select Password Criteria");
   
@@ -25,7 +26,7 @@ while (passLength < 8 || passLength > 128) {
   
   if (confirm ("Do you want uppercase characters")){
     characters.push(upperCase);
-  }
+}
   
   if (confirm ("Do you want numeric characters?")) {
   characters.push(numbers);
@@ -33,7 +34,10 @@ while (passLength < 8 || passLength > 128) {
 
   if (confirm ("Do you want special characters?")) {
   characters.push(special);
-  }
+}
+
+
+// Generate Password  
 
 for (var i= 0; i < passLength; i++) {
 password.push(characters[Math.floor(Math.random()*characters.length)]);
@@ -43,61 +47,7 @@ function generatePass () {
   document.getElementById("password").value = password.join(" ");
 }
 
-
-// Add event listener to generate button
+// Generate Button
 
 document.getElementById("generate").addEventListener("click", generatePass);
 
-
-
-// console.log(lowerCase)
-// console.log(upperCase)
-// console.log(numbers)
-// console.log(special)
-// console.log(passLength)
-// console.log(generateBtn)
-
-// function generateBtn () {
-
-// var password = generateBtn();
-// var passwordText = document.querySelector("#password");
-
-// passwordText.value = password;
-
-
-
-// Functions
-
-
-// for (var i=0, i < passLength , i++)
-
-
-// function passwordParameters() {
-  //   while (passLength < 8 || passLength > 128) {
-  //   passLength = prompt("Choose a password length between 8-128 characters")
-
-  // lowerCase = confirm ("Do you want lowercase characters?")
-
-  // upperCase = confirm ("Do you want uppercase characters")
-
-  // numbers = confirm ("Do you want numeric characters?")
-
-  // special = confirm ("Do you want special characters?")
-
-  //   }
-  // }
-
-// function writePassword() {
-//   var password = generate
-// }
-
-
-
-
-
-// Prompts to ask
-// Select the password criteria (alert)
-// Choose a password length between 8-128 characters: (insert number)
-// You must select at least one character type: Lowercase, uppercase, numeric, and/or special characters
-// If no character critera is selected put an alert(please enter valid answer)
-// Write password to the #password input
